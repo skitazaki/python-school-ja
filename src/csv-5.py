2c2a12615e54820037cd87bd1bb8b669614785cd
Shigeru Kitazaki <skitazaki@gmail.com>
2014-06-08 15:18:40 +0900

    Update CSV script rewriting chapter 5

--- src/csv-5.py
+++ src/csv-5.py
@@ -1,39 +1,77 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""%prog [options] csv_file
+"""Parse daily Tokyo stock prices.
 """
 
-import os
-
-from pyschool.unicodecsv import UnicodeReader
-from pyschool.cmdline import parse_args
-
-DEFAULT_CSV_FILE = 'sample.csv'
+import argparse
+import csv  # import standard "csv" module
+import logging
 
 
-def csv2tsv(fname):
-    """Convert a CSV format file to TSV format string.
+class StockPrice(object):
+
+    def __init__(self, day, price_begin, price_max, price_min, price_end):
+        self.day = day
+        self.price_begin = float(price_begin)
+        self.price_max = float(price_max)
+        self.price_min = float(price_min)
+        self.price_end = float(price_end)
+
+    def diff(self):
+        return self.price_end - self.price_begin
+
+
+def parse_args():
+    """Parse arguments and set up logging verbosity.
+
+    :rtype: parsed arguments as Namespace object.
     """
-    with open(fname) as reader:
-        stream = UnicodeReader(reader)
-        # Read a header line.
-        fields = stream.next()
-        for row in stream:
-            record = dict(zip(fields, row))
-            print record['rank'] + '\t' + \
-                  record['team'] + '\t' + \
-                  record['point'] + '\t' + \
-                  record['match'] + '\t' + \
-                  record['goaldiff']
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--file", dest="filename",
+                        help="setting file", metavar="FILE")
+    parser.add_argument("-o", "--output", dest="output",
+                        help="output file", metavar="FILE")
+    parser.add_argument("-n", "--dryrun", dest="dryrun",
+                        help="dry run", default=False, action="store_true")
+    parser.add_argument("-v", "--verbose", dest="verbose", default=False,
+                        action="store_true", help="verbose mode")
+    parser.add_argument("-q", "--quiet", dest="quiet", default=False,
+                        action="store_true", help="quiet mode")
+    # Add this line from boilerplate.
+    parser.add_argument("filename", nargs=1, help="CSV file path")
+
+    args = parser.parse_args()
+
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+    elif not args.quiet:
+        logging.basicConfig(level=logging.INFO)
+
+    return args
+
+
+def process(args):
+    """Parse daily Tokyo stock prices, and calculate up/down.
+    """
+    with open(args.filename[0]) as fp:
+        reader = csv.reader(fp)  # Instantiate CSV reader with file pointer.
+        for t in reader:
+            p = StockPrice(*t)
+            diff = p.diff()
+            if diff > 0:
+                message = 'up'
+            elif diff < 0:
+                message = 'down'
+            else:
+                message = 'same'
+            # Write out day, up/down/same, and diff.
+            print('{}\t{:5}\t{}'.format(p.day, message, round(diff, 2)))
 
 
 def main():
-    opts, args = parse_args(__doc__)
-    fname = args[0] if args else DEFAULT_CSV_FILE
-    if not os.path.exists(fname):
-        raise SystemExit("\"%s\" is not found." % (fname,))
-    csv2tsv(fname)
+    args = parse_args()
+    process(args)
 
 
 def test():
--- src/pyschool/cmdline.py
+++ src/pyschool/cmdline.py
@@ -3,30 +3,42 @@
 """Simple Command Line Utility.
 """
 
+import argparse
 import logging
-import optparse
 
 
-def parse_args(doc=None):
-    """Parse arguments and sets up logging verbosity.
+def parse_args():
+    """Parse arguments and set up logging verbosity.
 
-    :rtype: normal options and arguments as tuple.
+    :rtype: parsed arguments as Namespace object.
     """
-    parser = optparse.OptionParser(doc)
-    parser.add_option("-o", "--output", dest="output",
-        help="output file", metavar="FILE")
-    parser.add_option("-v", "--verbose", dest="verbose",
-        default=False, action="store_true", help="verbose mode")
-    parser.add_option("-q", "--quiet", dest="quiet",
-        default=False, action="store_true", help="quiet mode")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--file", dest="filename",
+                        help="setting file", metavar="FILE")
+    parser.add_argument("-o", "--output", dest="output",
+                        help="output file", metavar="FILE")
+    parser.add_argument("-n", "--dryrun", dest="dryrun",
+                        help="dry run", default=False, action="store_true")
+    parser.add_argument("-v", "--verbose", dest="verbose", default=False,
+                        action="store_true", help="verbose mode")
+    parser.add_argument("-q", "--quiet", dest="quiet", default=False,
+                        action="store_true", help="quiet mode")
+    # Add this line from boilerplate.
+    parser.add_argument("--header", dest="header", default=False,
+                        action="store_true", help="contains header row")
+    parser.add_argument("--encoding", dest="encoding", default='utf-8',
+                        help="encoding of input file")
+    parser.add_argument("-o", "--output", dest="output", metavar="FILE",
+                        help="output file path")
+    parser.add_argument("filename", nargs=1, help="input file path")
 
-    opts, args = parser.parse_args()
+    args = parser.parse_args()
 
-    if opts.verbose:
+    if args.verbose:
         logging.basicConfig(level=logging.DEBUG)
-    elif not opts.quiet:
+    elif not args.quiet:
         logging.basicConfig(level=logging.INFO)
 
-    return opts, args
+    return args
 
 # vim: set et ts=4 sw=4 cindent fileencoding=utf-8 :
