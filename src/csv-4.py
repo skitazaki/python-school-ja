0e818511f3462bd6ead7712978f17e2d1fc0513f
Shigeru Kitazaki <skitazaki@gmail.com>
2014-06-08 12:19:50 +0900

    Update CSV script to cleanup zip/dict explaination

--- src/csv-4.py
+++ src/csv-4.py
@@ -1,37 +1,84 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""%prog [options] csv_file
+"""Parse daily Tokyo stock prices.
 """
 
-import os
+import argparse
+import csv  # import standard "csv" module
+import logging
 
-from pyschool.unicodecsv import UnicodeReader
-from pyschool.cmdline import parse_args
+# Declare fields definition.
+FIELDS = (
+    {'id': 'day', 'type': 'string'},
+    {'id': 'price_begin', 'type': 'float'},
+    {'id': 'price_max', 'type': 'float'},
+    {'id': 'price_min', 'type': 'float'},
+    {'id': 'price_end', 'type': 'float'}
+)
 
-DEFAULT_CSV_FILE = 'sample.csv'
-LEAGUE_STATS = ('rank', 'team', 'point', 'match', 'goaldiff')
 
+def parse_args():
+    """Parse arguments and set up logging verbosity.
 
-def csv2tsv(fname):
-    """Convert a CSV format file to TSV format string.
+    :rtype: parsed arguments as Namespace object.
     """
-    with open(fname) as reader:
-        for row in UnicodeReader(reader):
-            record = dict(zip(LEAGUE_STATS, row))
-            print record['rank'] + '\t' + \
-                  record['team'] + '\t' + \
-                  record['point'] + '\t' + \
-                  record['match'] + '\t' + \
-                  record['goaldiff']
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--file", dest="filename",
+                        help="setting file", metavar="FILE")
+    parser.add_argument("-o", "--output", dest="output",
+                        help="output file", metavar="FILE")
+    parser.add_argument("-n", "--dryrun", dest="dryrun",
+                        help="dry run", default=False, action="store_true")
+    parser.add_argument("-v", "--verbose", dest="verbose", default=False,
+                        action="store_true", help="verbose mode")
+    parser.add_argument("-q", "--quiet", dest="quiet", default=False,
+                        action="store_true", help="quiet mode")
+    # Add this line from boilerplate.
+    parser.add_argument("--header", dest="header", default=False,
+                        action="store_true", help="contains header row")
+    parser.add_argument("--encoding", dest="encoding", default='utf-8',
+                        help="Encoding of input file")
+    parser.add_argument("filename", nargs=1, help="CVS file path")
+
+    args = parser.parse_args()
+
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+    elif not args.quiet:
+        logging.basicConfig(level=logging.INFO)
+
+    return args
+
+
+def process(args):
+    """Parse daily Tokyo stock prices, and calculate up/down.
+    """
+    header = tuple(map(lambda f: f['id'], FIELDS))
+    with open(args.filename[0], encoding=args.encoding) as fp:
+        reader = csv.reader(fp)  # Instantiate CSV reader with file pointer.
+        if args.header:
+            _ = next(reader)
+            logging.info('Skip header row: %s', _)
+        else:
+            logging.info('No header row is to be in input file.')
+        for t in reader:
+            dt = dict(zip(header, t))  # Bind header and row
+            # Calculate the differenciate of the day.
+            diff = float(dt['price_end']) - float(dt['price_begin'])
+            if diff > 0:
+                message = 'up'
+            elif diff < 0:
+                message = 'down'
+            else:
+                message = 'same'
+            # Write out day, up/down/same, and diff.
+            print('{}\t{:5}\t{}'.format(dt['day'], message, round(diff, 2)))
 
 
 def main():
-    opts, args = parse_args(__doc__)
-    fname = args[0] if args else DEFAULT_CSV_FILE
-    if not os.path.exists(fname):
-        raise SystemExit("\"%s\" is not found." % (fname,))
-    csv2tsv(fname)
+    args = parse_args()
+    process(args)
 
 
 def test():
